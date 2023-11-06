0ca906b2dba0b195b525117b1edd1def891e1b42
leomayer <leo.zen88@gmail.com>
2023-11-06 15:08:28 +0100

    commited plugin

--- plugin/zen-calendar/zen-calendar.php
+++ plugin/zen-calendar/zen-calendar.php
@@ -15,7 +15,7 @@ $zencal_db_version = '1.0';
 function load_ng_scripts()
 {
     wp_enqueue_style('ng_styles', plugin_dir_url(__FILE__) . 'dist/styles.463c5bcc279f9804.css');
-    wp_register_script('ng_main', plugin_dir_url(__FILE__) . 'dist/main.7abc61f60dd74e88.js', true);
+    wp_register_script('ng_main', plugin_dir_url(__FILE__) . 'dist/main.849a5fca524b2746.js', true);
     wp_register_script('ng_polyfills', plugin_dir_url(__FILE__) . 'dist/polyfills.7ef82dbfc6acbeb8.js', true);
     wp_register_script('ng_runtime', plugin_dir_url(__FILE__) . 'dist/runtime.d828c3a65864714d.js', true);
 }
